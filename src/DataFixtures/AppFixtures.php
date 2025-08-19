bbdf01cbf145530ba9d8e4ff268ef84ca0b0a50a
Emile Deballon <mr.scrupulus@gmail.com>
2025-08-19 13:51:36 +0200

    user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,28 +5,54 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Book;
-use App\Entity\Author;  
+use App\Entity\Author;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
-    {
-      $listAuthor = [];
-      for($i = 0; $i < 10; $i++){
-        $author = new Author();
-        $author->setFirstName('Prénom ' . $i);
-        $author->setLastName('Nom ' . $i);
-        $manager->persist($author);
-        $listAuthor[] = $author;
-      }
+  private $userPasswordHasher;
 
-        for ($i = 0; $i < 10; $i++) {
-            $livre = new Book;
-            $livre->setTitle('Livre ' . $i);
-            $livre->setCoverText('Quatrième de couverture numéro :' . $i);
-            $livre->setAuthor($listAuthor[array_rand($listAuthor)]);
-            $manager->persist($livre);
-        }
-        $manager->flush();
+  public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+  {
+    $this->userPasswordHasher = $userPasswordHasher;
+  }
+
+  public function load(ObjectManager $manager): void
+  {
+    // Création d'un user "normal"
+    $user = new User();
+    $user->setEmail("user@bookapi.com");
+    $user->setRoles(["ROLE_USER"]);
+    $user->setPassword($this->userPasswordHasher->hashPassword($user, "password"));
+    $manager->persist($user);
+
+    // Création d'un user admin
+    $userAdmin = new User();
+    $userAdmin->setEmail("admin@bookapi.com");
+    $userAdmin->setRoles(["ROLE_ADMIN"]);
+    $userAdmin->setPassword($this->userPasswordHasher->hashPassword($userAdmin, "password"));
+    $manager->persist($userAdmin);
+
+    // Création des auteurs
+    $listAuthor = [];
+    for ($i = 0; $i < 10; $i++) {
+      $author = new Author();
+      $author->setFirstName('Prénom ' . $i);
+      $author->setLastName('Nom ' . $i);
+      $manager->persist($author);
+      $listAuthor[] = $author;
     }
+
+    // Création des livres
+    for ($i = 0; $i < 10; $i++) {
+      $livre = new Book();
+      $livre->setTitle('Livre ' . $i);
+      $livre->setCoverText('Quatrième de couverture numéro :' . $i);
+      $livre->setAuthor($listAuthor[array_rand($listAuthor)]);
+      $manager->persist($livre);
+    }
+
+    $manager->flush();
+  }
 }
